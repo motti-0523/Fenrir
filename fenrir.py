68576811f74a6e855f05314cdd14ce5844513238
motti-0523 <138700869+motti-0523@users.noreply.github.com>
2023-07-06 18:17:26 +0900

    Update fenrir.py

--- fenrir.py
+++ fenrir.py
@@ -6,12 +6,13 @@ def isValid(brackets):
 
     for char in brackets:
         if char in opening_brackets:
+            #開き括弧をスタックに格納
             stack.append(char)
         elif char in closing_brackets:
-            #開き括弧があった際に、スタックからポップさせて一致するかを確認
+            #スタック内に開き括弧があるか、開き括弧があった際に、スタックからポップさせて一致するかを確認
             if not stack or brackets_map[char] != stack.pop():
                 return False
-
+    #スタック内に開き括弧が残っていないか＝すべての開き括弧に対応する閉じ括弧があるかを確認
     return len(stack) == 0
 
 while True:
